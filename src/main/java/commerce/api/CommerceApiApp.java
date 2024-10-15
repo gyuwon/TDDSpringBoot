7935b2c9b51c04e366aa86b38a0a6fcdf479ac0c
Gyuwon Yi <gyuwon@live.com>
2024-10-15 22:50:00 +0900

    API 앱 프로젝트를 초기화한다

---
+++ src/main/java/commerce/api/CommerceApiApp.java
@@ -0,0 +1,12 @@
+package commerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceApiApp.class, args);
+    }
+}
